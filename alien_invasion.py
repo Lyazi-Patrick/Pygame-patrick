4070d6bd0670699a0dbc35bcfc71814ec3de3b6c
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-05-22 20:36:10 -0700

    modified the speed of the ship and also limited its range and also enabled its movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -35,6 +36,26 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                     self._check_keydown_events(event)
+
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+    
+    def _check_keydown_events(self,event):
+        """Respond to Keypresses."""
+        if event.key == pygame.K_RIGHT:
+             self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+             self.ship.moving_left = True
+    
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+                    
     def _update_screen(self):
             """Update images on the screen, and flip to the new screen"""
             #Redraw the screen during each pass through the loop.
