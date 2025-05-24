42050627d64258d1fa1a2f89618cc7c5bf927990
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-05-24 00:59:09 -0700

    Added Bullet settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,10 +12,13 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((
+        """self.screen = pygame.display.set_mode((
             self.settings.screen_width, self.settings.screen_height
         ))
-        pygame.init()
+        pygame.init()"""
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         
         #Set the background color.
         self.bg_color = (230,230,230)
@@ -48,6 +51,8 @@ class AlienInvasion:
              self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
              self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+             sys.exit()
     
     def _check_keyup_events(self, event):
         """Respond to key releases."""
