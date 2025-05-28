53566d1f13f6171ceaeb4dbe0be545146237186f
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-05-28 02:31:08 -0700

    Modified code to ensure that aliens can now be shot and also the dissapear plus the bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -95,6 +95,9 @@ class AlienInvasion:
             #print(len(self.bullets))
 
             self._update_screen()
+            #Check for any bullets that have hit aliens.
+            #if so, get rid of the bullet and the alien.
+            collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True,True)
     
     def _update_aliens(self):
          """Check if the fleet is at the edge,Update the positions of all aliens in the fleet."""
