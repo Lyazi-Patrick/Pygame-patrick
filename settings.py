d86e0ecdc8f9e9b0e8dae0d109021a5bf45f874c
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-05-29 03:01:29 -0700

    Increased the speed of the bullets

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_speed = 1.5
         
         #Bullet settings
-        self.bullet_speed = 0.9
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 20
         self.bullet_color = (60,60,60)
