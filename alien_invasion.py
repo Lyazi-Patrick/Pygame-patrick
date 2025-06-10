09d1ca18f5d7851506864982c9545b101fd0ddcf
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-06-10 18:21:32 +0300

    Added a new folder and also modified some files to improve on game quality

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
+from time import sleep
 
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -26,6 +28,8 @@ class AlienInvasion:
         self.bg_color = (230,230,230)
 
         pygame.display.set_caption('Alien Invasion')
+        #Create an instance to store game statistics.
+        self.stats = GameStats(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -113,7 +117,26 @@ class AlienInvasion:
          self.aliens.update()
          #look for alien-ship collisions.
          if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print('Ship Hit!')
+            self._ship_hit()   
+        #look for aliens hitting the bottom of the screen.
+            self._check_aliens_bottom()
+    
+    def _ship_hit(self):
+         """Respond to the ship being hit by an alien."""
+         #Decrement ships_left.
+         self.stats.ships_left -= 1
+
+         #Get rid of any remaining aliens and bullets.
+         self.aliens.empty()
+         self.bullets.empty()
+
+         #Create a new fleet and center the ship.
+         self._create_fleet()
+         self.ship.center_ship()
+
+         #pause.
+         sleep(0.5)
+        
 
     def _check_events(self):
             """Respond to keypresses and mouse events."""
@@ -149,7 +172,16 @@ class AlienInvasion:
          """Create a new bullet and add it to the bullets group."""
          if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)                
+            self.bullets.add(new_bullet) 
+
+    def _check_aliens_bottom(self):
+         """Check if any aliens have reached the bottom of screen."""
+         screen_rect = self.screen.get_rect()
+         for alien in self.aliens.sprites():
+              if alien.rect.bottom >= screen_rect.bottom:
+                   #Treat this the same as if the ship got hit.
+                   self._ship_hit()
+                   break               
     def _update_screen(self):
             """Update images on the screen, and flip to the new screen"""
             #Redraw the screen during each pass through the loop.
