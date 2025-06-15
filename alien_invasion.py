cd582d07ff157c0375b88f0a52fca2bb7b4af79c
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-06-15 16:00:50 +0300

    Hid the Mouse Cursor when the game starts

--- alien_invasion.py
+++ alien_invasion.py
@@ -164,7 +164,8 @@ class AlienInvasion:
     
     def _check_play_button(self, mouse_pos):
          """Start a new game when the player clicks play."""
-         if self.play_button.rect.collidepoint(mouse_pos):
+         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+         if button_clicked and not self.stats.game_active:
               #Reset the game statistics
               self.stats.reset_stats()
               self.stats.game_active = True
@@ -176,6 +177,9 @@ class AlienInvasion:
               #Create a new fleet and center the ship.
               self._create_fleet()
               self.ship.center_ship()
+
+              #Hide the mouse cursor.
+              pygame.mouse.set_visible(False)
     
     def _check_keydown_events(self,event):
         """Respond to Keypresses."""
