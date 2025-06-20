bf80d3ada82bf3bd6ff75b49c8553ed5afc122c6
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-06-20 20:38:00 +0300

    Updated the score as Aliens are shotdown

--- settings.py
+++ settings.py
@@ -36,6 +36,10 @@ class Settings:
 
         #Fleet_direction of 1 represents right;-1 represents left.
         self.fleet_direction = 1
+
+        #scoring
+        self.alien_points = 50
+
     
     def increase_speed(self):
         """Increase speed settings."""
